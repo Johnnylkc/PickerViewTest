b700d4f8fc21a315b4a8b9e1f9935e7d4ca40230
Johnnylkc <momozzz1231@gmail.com>
2016-03-04 18:52:20 +0800

    304 18:50

--- PickerViewTest/TestVC.swift
+++ PickerViewTest/TestVC.swift
@@ -12,11 +12,15 @@ class TestVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
     let tableView = UITableView()
     
+    let scrollBar = UIScrollView()
+    let scrollButton1 = UIButton()
+    let scrollButton2 = UIButton()
+    let scrollButton3 = UIButton()
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.lightGrayColor()
-
+        
         
         allUI()
         autoLayout()
@@ -25,22 +29,52 @@ class TestVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
     func allUI()
     {
-        self.tableView.frame = CGRectMake(0, 170, self.view.frame.size.width, 400)
-        self.tableView.backgroundColor = UIColor.whiteColor()
-        self.tableView.delegate = self
-        self.tableView.dataSource = self
-        self.automaticallyAdjustsScrollViewInsets = false
-        self.tableView.registerClass(MainCell.self, forCellReuseIdentifier: "cell")
+        
+        self.view.backgroundColor = UIColor.greenColor()
+        
+        tableView.backgroundColor = UIColor.greenColor()
+        tableView.delegate = self
+        tableView.dataSource = self
+        automaticallyAdjustsScrollViewInsets = false
+        tableView.registerClass(MainCell.self, forCellReuseIdentifier: "cell")
         self.view.addSubview(self.tableView)
         
+    
+        scrollBar.backgroundColor = UIColor.redColor()
+        scrollBar.contentSize = CGSizeMake(self.view.frame.size.width*2, 0)
+        self.view.addSubview(scrollBar)
         
+        scrollButton1.frame = CGRectMake(10, 5, 100, 30)
+        scrollButton1.backgroundColor = UIColor.whiteColor()
+        scrollButton1.setTitle("按鈕1", forState: .Normal)
+        scrollButton1.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        scrollBar.addSubview(scrollButton1)
+        
+        scrollButton2.frame = CGRectMake(120, 5, 100, 30)
+        scrollButton2.backgroundColor = UIColor.whiteColor()
+        scrollButton2.setTitle("按鈕2", forState: .Normal)
+        scrollButton2.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        scrollBar.addSubview(scrollButton2)
+        
+        scrollButton3.frame = CGRectMake(230, 5, 100, 30)
+        scrollButton3.backgroundColor = UIColor.whiteColor()
+        scrollButton3.setTitle("按鈕3", forState: .Normal)
+        scrollButton3.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        scrollBar.addSubview(scrollButton3)
+    
+    
     }
     
     func autoLayout()
     {
         tableView.translatesAutoresizingMaskIntoConstraints = (false)
+        scrollBar.translatesAutoresizingMaskIntoConstraints = (false)
         
-        let dic = ["tableView":tableView]
+//        scrollButton1.translatesAutoresizingMaskIntoConstraints = (false)
+//        scrollButton2.translatesAutoresizingMaskIntoConstraints = (false)
+//        scrollButton3.translatesAutoresizingMaskIntoConstraints = (false)
+        
+        let dic = ["tableView":tableView,"scrollBar":scrollBar]
         
         ////tableView
         let tableViewH = NSLayoutConstraint.constraintsWithVisualFormat("H:|[tableView]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: dic)
@@ -48,6 +82,30 @@ class TestVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
         
         let tableViewV = NSLayoutConstraint.constraintsWithVisualFormat("V:|-110-[tableView]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: dic)
         self.view.addConstraints(tableViewV)
+        
+        
+        ////scrollBar
+        let scrollBarH = NSLayoutConstraint.constraintsWithVisualFormat("H:|[scrollBar]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: dic)
+        self.view.addConstraints(scrollBarH)
+        
+        let scrollBarV = NSLayoutConstraint.constraintsWithVisualFormat("V:|-64-[scrollBar][tableView]", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: dic)
+        self.view.addConstraints(scrollBarV)
+        
+//        ////scrollButton水平之間的距離和寬度
+//        let scrollButtonsH = NSLayoutConstraint.constraintsWithVisualFormat("H:|-10-[scrollButton1]-10-[scrollButton2]-10-[scrollButton3]-10-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: dic)
+//        scrollBar.addConstraints(scrollButtonsH)
+//        
+//        ////scrollButton1的垂直位置和高度
+//        let scrollButton1V = NSLayoutConstraint.constraintsWithVisualFormat("V:|[scrollButton1]", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: dic)
+//        scrollBar.addConstraints(scrollButton1V)
+//        
+//        ////scrollButton2的垂直位置和高度
+//        let scrollButton2V = NSLayoutConstraint.constraintsWithVisualFormat("V:|[scrollButton2]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: dic)
+//        scrollBar.addConstraints(scrollButton2V)
+//        
+//        ////scrollButton3的垂直位置和高度
+//        let scrollButton3V = NSLayoutConstraint.constraintsWithVisualFormat("V:|[scrollButton3]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: dic)
+//        scrollBar.addConstraints(scrollButton3V)
         
         
     }
@@ -72,7 +130,7 @@ class TestVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! MainCell
-        
+        cell.selectionStyle = .None
         //cell.textLabel?.text = "ggg"
         
         return cell
